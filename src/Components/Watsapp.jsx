0079a8cc260f8cd36e7cb812a92e8742848c4059
MFHIITP <118089487+MFHIITP@users.noreply.github.com>
2024-07-15 14:55:21 +0530

    Update Watsapp.jsx

--- src/Components/Watsapp.jsx
+++ src/Components/Watsapp.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Watsapp(props) {
   return (
-    <div className="fixed bottom-1 right-2 text-white bg-gray-800 rounded-lg px-4 py-1 mx-8 text-xs z-20 hover:bg-gray-900 opacity-95">
+    <div className="fixed bottom-1 right-0 text-white bg-gray-800 rounded-lg px-4 py-1 mx-8 text-xs z-20 hover:bg-gray-900 opacity-95">
       <div className={`${(props.registered === false) ? "hidden" : "none"}`}>
-        <a href="https://chat.whatsapp.com/H7shXB5TJtK2XcUbvRoujO" className="flex gap-8">
-          <div className="flex items-center font-extralight">
+        <a href="https://chat.whatsapp.com/H7shXB5TJtK2XcUbvRoujO" className="md:flex gap-8">
+          <div className="flex items-center font-extralight w-6 md:w-full">
             Join our Official Whatsapp Group
           </div>
           <div className="text-xs font-extralight">
@@ -14,6 +14,7 @@ function Watsapp(props) {
               alt="Image"
               height={30}
               width={30}
+              className="ml-4"
             />
             Whatsapp
           </div>
